6449e1ab4f0d876e9eee6a1790bb97d953be0c72
cmullison <132401969+cmullison@users.noreply.github.com>
2024-07-26 14:46:31 -0400

    more prisma

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL ?? "",
+    DIRECT_URL: process.env.DIRECT_URL ?? "",
+    RESEND_API_KEY: process.env.RESEND_API_KEY ?? "",
+  },
     images: {
     domains: ['hephebyszkkaamlckwjm.supabase.co'], // Add your Supabase storage domain here
   },
