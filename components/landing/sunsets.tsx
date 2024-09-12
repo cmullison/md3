37039fc8b48a5f74c00fe4cee1ecdb40ad2e175c
cmullison <132401969+cmullison@users.noreply.github.com>
2024-09-12 16:31:05 -0400

    lp3

--- components/landing/sunsets.tsx
+++
@@ -1,9 +0,0 @@
-import Spline from "@splinetool/react-spline/next";
-
-export default function Home() {
-  return (
-    <main>
-      <Spline scene="https://prod.spline.design/AMWu-tdgQwHlskdQ/scene.splinecode" />
-    </main>
-  );
-}
